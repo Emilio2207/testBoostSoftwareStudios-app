52a7fe68c675e89c947ebe1ff58e86821909f8e5
EmilioRamirez <emi.emilioramirez@gmail.com>
2024-08-25 16:41:50 -0300

    update test

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Guía Video
 
-[guia (tiempo: 4:35:29)](https://www.youtube.com/watch?v=ZF73dpgRrWI&t=59s)
+[guia (tiempo: 4:56:20)](https://www.youtube.com/watch?v=ZF73dpgRrWI&t=59s)
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div></div>;
+  const [currentState, setCurrentState] = useState("Inscribirse");
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {currentState === "Acceso" ? (
+        ""
+      ) : (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800"
+          placeholder="Nombre"
+          required
+        />
+      )}
+      <input
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Contraseña"
+        required
+      />
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+        <p className="cursor-pointer">¿Olvidaste tu contraseña?</p>
+        {currentState === "Acceso" ? (
+          <p onClick={() => setCurrentState("Inscribirse")} className="cursor-pointer">
+            Crear una cuenta
+          </p>
+        ) : (
+          <p onClick={() => setCurrentState("Acceso")} className="cursor-pointer">
+            Inicia sesión aquí
+          </p>
+        )}
+      </div>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4">
+        {currentState === "Acceso" ? "Iniciar" : "Inscribirse"}
+      </button>
+    </form>
+  );
 };
 
 export default Login;
